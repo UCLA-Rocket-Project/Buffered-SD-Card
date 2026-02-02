1210ddefd1eeedf66827738b21a679a123c0fde0
SleepyWoodpecker <shawnweichew@gmail.com>
2026-02-02 00:25:56 -0800

    feat: added more utility functions to count and delete files

--- buffered_sd.cpp
+++ buffered_sd.cpp
@@ -1,9 +1,12 @@
-#include <buffered_sd.h>
 #include <SD.h>
+#include <buffered_sd.h>
 
-BufferedSD::BufferedSD(SPIClass &spi_bus, uint8_t CS, const char *base_filepath, const char *extension, size_t buffer_size) 
-    :_spi(&spi_bus), _CS_pin(CS), _buffer_size(buffer_size), _base_path(base_filepath), _extension(extension)
-{
+BufferedSD::BufferedSD(
+    SPIClass &spi_bus, uint8_t CS, const char *base_filepath, const char *extension,
+    size_t buffer_size
+)
+    : _spi(&spi_bus), _CS_pin(CS), _buffer_size(buffer_size), _base_path(base_filepath),
+      _extension(extension) {
     // add the () behind to zero-initialize the buffer
     _write_buffer = new uint8_t[buffer_size]();
     configASSERT(_write_buffer && "Failed to initialize write buffer");
@@ -19,15 +22,14 @@ bool BufferedSD::begin() {
     for (int i = 0; i < NUM_TRIES_TO_OPEN; ++i) {
         if (SD.begin(_CS_pin, *_spi)) {
             break;
-        }
-        else if (i == NUM_TRIES_TO_OPEN - 1) {
+        } else if (i == NUM_TRIES_TO_OPEN - 1) {
             return false;
         }
         delay(100);
     }
 
     find_first_available_file(_base_path, _extension);
-    
+
     File f = SD.open(_filepath, FILE_WRITE);
     if (!f) {
         Serial.print("Failed to create file: ");
@@ -48,7 +50,7 @@ int BufferedSD::write(const char *data) {
 
     memcpy(_write_buffer + _buffer_idx, data, length);
     _buffer_idx += length;
-    
+
     return length;
 }
 
@@ -60,7 +62,7 @@ int BufferedSD::write(const char *data, size_t length) {
 
     memcpy(_write_buffer + _buffer_idx, data, length);
     _buffer_idx += length;
-    
+
     return length;
 }
 
@@ -70,7 +72,8 @@ int BufferedSD::write_immediate(const char *data) {
     size_t length = strlen(data);
 
     File f = SD.open(_filepath, FILE_APPEND);
-    if (!f) return -1;
+    if (!f)
+        return -1;
     size_t written_length = 0;
     written_length += f.write(reinterpret_cast<const uint8_t *>(data), strlen(data));
     f.close();
@@ -82,7 +85,8 @@ int BufferedSD::write_immediate(const char *data, size_t length) {
     flush_buffer();
 
     File f = SD.open(_filepath, FILE_APPEND);
-    if (!f) return -1;
+    if (!f)
+        return -1;
     size_t written_length = 0;
     written_length += f.write(reinterpret_cast<const uint8_t *>(data), length);
     f.close();
@@ -111,10 +115,7 @@ void BufferedSD::find_first_available_file(const char *planned_filepath, const c
         // FAT32 (file format used by XTSD has a limit of 256 files per directory)
         for (int i = 0; i < 256; ++i) {
             snprintf(
-                temp_filepath, 
-                FILEPATH_NAME_MAX_LENGTH, 
-                "/%s-%d%s", 
-                planned_filepath, i, extension
+                temp_filepath, FILEPATH_NAME_MAX_LENGTH, "/%s-%d%s", planned_filepath, i, extension
             );
 
             if (!SD.exists(temp_filepath)) {
@@ -134,7 +135,7 @@ sd_card_update BufferedSD::get_file_update() {
     }
 
     uint32_t file_size = file.size();
-   
+
     // skip past the final \n character
     uint32_t offset = file_size - 3;
     file.seek(offset, SeekSet);
@@ -163,12 +164,49 @@ sd_card_update BufferedSD::get_file_update() {
 
     char *endptr;
 
-    return {
-        file_size,
-        strtoul(number, &endptr, 10)
-    };
+    return {file_size, strtoul(number, &endptr, 10)};
 }
 
-void BufferedSD::get_file_name(char *buf) {
-    strncpy(buf, _filepath, strlen(_filepath) + 1);
+void BufferedSD::get_file_name(char *buf) { strncpy(buf, _filepath, strlen(_filepath) + 1); }
+
+bool BufferedSD::delete_all_files() {
+    File root = SD.open("/");
+    if (!root) {
+        return false;
+    }
+
+    File file = root.openNextFile();
+
+    while (file) {
+        char name[FILEPATH_NAME_MAX_LENGTH];
+        snprintf(name, FILEPATH_NAME_MAX_LENGTH, "/%s", file.name());
+        deleteFile(SD, name);
+
+        file = root.openNextFile();
+
+        // add timeout so there is no watchdog reset
+        delay(1);
+    }
+
+    return true;
+}
+
+int BufferedSD::count_top_level_files() {
+    int counter = 0;
+
+    File root = SD.open("/");
+    if (!root) {
+        return -1;
+    }
+    if (!root.isDirectory()) {
+        return -2;
+    }
+
+    File file = root.openNextFile();
+    while (file) {
+        counter++;
+        file = root.openNextFile();
+    }
+
+    return counter;
 }
--- buffered_sd.h
+++ buffered_sd.h
@@ -1,31 +1,34 @@
 #ifndef BUFFERED_SD_H_
 #define BUFFERED_SD_H_
 
-#include <stdint.h>
-#include <SPI.h>
 #include <SD.h>
+#include <SPI.h>
+#include <stdint.h>
 
-#define DEFAULT_BUFFER_SIZE 4096
+#define DEFAULT_BUFFER_SIZE      4096
 #define FILEPATH_NAME_MAX_LENGTH 128
-#define NUM_TRIES_TO_OPEN 5
-#define SD_IDLE_TIMEOUT 5000UL
+#define NUM_TRIES_TO_OPEN        5
+#define SD_IDLE_TIMEOUT          5000UL
 
 struct sd_card_update {
     uint32_t file_size;
     uint32_t last_written_timestamp;
 };
 
-class BufferedSD 
-{
-public:
+class BufferedSD {
+  public:
     /**
      * @param spi_bus: the custom SPI bus
      * @param CS: the CS pin
-     * @param base_filepath: the base filepath that will be used to find the next available file to write to
+     * @param base_filepath: the base filepath that will be used to find the next
+     * available file to write to
      * @param extension: the file extension of the log file
      * @param buffer_size: the size of the write buffer
-    */
-    BufferedSD(SPIClass &spi_bus, uint8_t CS, const char *base_filepath = "", const char *extension = ".txt", size_t buffer_size = DEFAULT_BUFFER_SIZE);
+     */
+    BufferedSD(
+        SPIClass &spi_bus, uint8_t CS, const char *base_filepath = "",
+        const char *extension = ".txt", size_t buffer_size = DEFAULT_BUFFER_SIZE
+    );
     ~BufferedSD();
 
     bool begin();
@@ -37,44 +40,46 @@ public:
 
     /**
      * @brief: function specific to the debugging module on the esp32s
-     * 
+     *
      * @returns: file size + timestamp of last written log entry
      */
     sd_card_update get_file_update();
 
     void get_file_name(char *buf);
 
+    bool delete_all_files();
+    int count_top_level_files();
+
     /**
-     * @brief: find the first available file name, which is achieved by adding a number to the end of the current file name
-     * 
+     * @brief: find the first available file name, which is achieved by adding
+     * a number to the end of the current file name
+     *
      * @param planned_filepath: the start filepath to search from
      * @param final_filepath: the first file name that is found
      * @param extension: the file extension
      */
     void find_first_available_file(const char *planned_filepath, const char *extension);
-    
+
     inline bool has_buffered_data();
     inline void flush_buffer();
 
-private:
+  private:
     uint8_t *_write_buffer;
     size_t _buffer_idx;
     size_t _buffer_size;
     char _filepath[FILEPATH_NAME_MAX_LENGTH];
     const char *_base_path;
     const char *_extension;
-    
+
     SPIClass *_spi;
     uint8_t _CS_pin;
 };
 
-
-inline bool BufferedSD::has_buffered_data() {
-    return _buffer_idx > 0;
-}
+inline bool BufferedSD::has_buffered_data() { return _buffer_idx > 0; }
 
 inline void BufferedSD::flush_buffer() {
-    if (_buffer_idx == 0) return;
+    if (_buffer_idx == 0)
+        return;
 
     File f = SD.open(_filepath, FILE_APPEND);
     if (!f) {
